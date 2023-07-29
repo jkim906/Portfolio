909a0e43c7de21bcd37b04ba288da15c79734eb9
Jaewon Kim <jaewon@wifi-172-23-50-79.uoa-wifi.auckland.ac.nz>
2023-07-29 17:16:24 +1200

    worked on portfolio

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,32 +1,32 @@
 import React from 'react'
-import Jae from "../images/Jae.jpeg";
+import Jae from "../images/Jae-profile.png";
 import { TiArrowRightOutline } from 'react-icons/ti'
 
 const Home = () => {
   return (
-    <div name="home" className="flex h-screen w-full bg-gradient-to-b from-black to-gray-800">
-      <div className="max-w-screen-lg mx-auto flex-col items-center justify-center h-full px-4 md:flex-row">
-        <div>
-          <h2>
+    <div name="home" className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800">
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+        <div className="flex flex-col justify-center h-full">
+          <h2 className="text-4xl sm:text-7xl font-bold text-white">
             I'm a full Stack Developer
           </h2>
-          <p>
+          <p className="text-gray-500 py-4 max-w-md">
             I'm a full stack developer with a passion for creating beautiful, responsive, and functional websites. I have experience in HTML, CSS, JavaScript, React, Node.js, Express, MongoDB, and more.
           </p>
-        <div>
-        <button>
-          Portfolio
-          <span>
-            <TiArrowRightOutline />
-          </span>
-        </button>
-      </div>
-    </div>
+          <div>
+            <button className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursur-pointer">
+              Portfolio
+              <span className="group-hover:rotate-90 duration-300 ml-2">
+                <TiArrowRightOutline size={18}/>
+              </span>
+            </button>
+          </div>
+        </div>
 
-    <div>
-      <img src={Jae} alt="my profile" />
-    </div>
-    </div>
+        <div>
+          <img src={Jae} alt="my profile" className="rounded-2xl mx-auto w-2/3 md:full"/>
+        </div>
+      </div>
     </div>
   );
 };
